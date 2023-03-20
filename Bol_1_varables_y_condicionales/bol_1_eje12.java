462396e244fe8a291948d6becf36603e7b6b8af6
Judadego <judadego@gmail.com>
2023-03-20 12:40:35 -0500

    second day

---
+++ Bol_1_varables_y_condicionales/bol_1_eje12.java
@@ -0,0 +1,48 @@
+package Bol_1_varables_y_condicionales;
+
+import java.util.Scanner;
+
+public class bol_1_eje12 {
+    public static void main(String[] Args) {
+        int a;
+        int dm, um, c, d, u;
+        System.out.println("Ingresa un número de máx. 5 cifras para mostrarlo con sus cifras al revés. ");
+        Scanner A = new Scanner(System.in);
+        a = A.nextInt();
+        if (a < 100000) {
+            if (a > 0) {
+                u = a % 10;
+                a = a / 10;
+                if (a > 0) {
+                    d = a % 10;
+                    a = a / 10;
+                    if (a > 0) {
+                        c = a % 10;
+                        a = a / 10;
+                        if (a > 0) {
+                            um = a % 10;
+                            a = a / 10;
+                            if (a > 0) {
+                                dm = a % 10;
+                                a = a / 10;
+                                System.out.println("" + u + d + c + um + dm);
+                            } else {
+                                System.out.println("" + u + d + c + um);
+                            }
+                        } else {
+                            System.out.println("" + u + d + c);
+                        }
+                    } else {
+                        System.out.println("" + u + d);
+                    }
+                } else {
+                    System.out.println(u);
+                }
+            } else {
+                System.out.println("Debe ingresar un numero mayor que cero.");
+            }
+        }else{
+            System.out.println("El numero es de más de 5 cifras. ");
+        }
+    }
+}
---
+++ Bol_1_varables_y_condicionales/bol_1_eje13.java
@@ -0,0 +1,58 @@
+package Bol_1_varables_y_condicionales;
+
+import java.util.Scanner;
+
+public class bol_1_eje13 {
+    public static void main(String[] Args) {
+        String num = "";
+        String  b;
+        int dm, um, c, d, u;
+        int a;
+        System.out.println("Ingresa un número de máx. 5 cifras para mostrarlo con sus cifras al revés. ");
+        Scanner A = new Scanner(System.in);
+        a = A.nextInt();
+        b = ""+ a;
+        if (a < 100000) {
+            if (a > 10) {
+                u = a % 10;
+                a = a / 10;
+                if (a > 0) {
+                    d = a % 10;
+                    a = a / 10;
+                    if (a > 0) {
+                        c = a % 10;
+                        a = a / 10;
+                        if (a > 0) {
+                            um = a % 10;
+                            a = a / 10;
+                            if (a > 0) {
+                                dm = a % 10;
+                                a = a / 10;
+                                num = "" + u + d + c + um + dm;
+                            } else {
+                                num = "" + u + d + c + um;
+                            }
+                        } else {
+                            num = "" + u + d + c;
+                        }
+                    } else {
+                        
+                        num = "" + u + d;
+                    }
+                } else {
+                    num = "" + u;
+                }
+            } else {
+                System.out.println("Debe ingresar un numero con mas de dos dígitos.");
+            }
+        }else{
+            System.out.println("El numero es de más de 5 cifras. ");
+        }
+
+        if(Integer.parseInt(num) == Integer.parseInt(b) ){
+            System.out.println("El numero "+ b +" es capicúa: " + num );
+        }else{
+            System.out.println("El número " + b + " no es Capicúa: " + num);
+        }
+    }
+}
